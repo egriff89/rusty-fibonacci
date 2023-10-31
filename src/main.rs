11e8f51848698f8cf8ceea1a4fd361d7098d37f4
Eric Griffith <eric.griffith42@gmail.com>
2023-10-31 04:31:29 -0500

    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
-use num::BigInt;
+use num_bigint::BigInt;
+use num_traits::{Zero, One};
 use std::cmp::Ordering;
-use anyhow::{Result, bail};
+use anyhow::bail;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -10,19 +11,18 @@ struct Args {
     number: i32
 }
 
-fn fib(nth: i32) -> Result<BigInt> {
-    
+fn fib(nth: i32) -> anyhow::Result<BigInt> {
     match nth.cmp(&0) {
-        Ordering::Equal   => Ok(BigInt::from(0)),
+        Ordering::Equal   => Ok(Zero::zero()),
         Ordering::Less    => bail!("Number cannot be negative!"),
         Ordering::Greater => {
-            let mut a: BigInt = BigInt::from(1);
-            let mut b: BigInt = BigInt::from(1);
+            let mut a: BigInt = One::one();
+            let mut b: BigInt = One::one();
 
             for _ in 1..nth {
-                let temp: BigInt = a;
-                a = b.clone();
-                b = b.checked_add(&temp).unwrap();
+                let temp: BigInt = a + &b;
+                a = b;
+                b = temp;
             }
             Ok(a)
         }
