731dac0aa9c299790cb3e5ca0a7278b9d5c6cad5
Eric Griffith <eric.griffith42@gmail.com>
2023-11-10 20:30:30 -0600

    Add function to format resulting number as a thousands-separated string

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
+use anyhow::bail;
 use clap::Parser;
 use num_bigint::BigInt;
-use num_traits::{Zero, One};
+use num_traits::{One, Zero};
 use std::cmp::Ordering;
-use anyhow::bail;
 
 #[derive(Parser, Debug)]
 #[command(about)]
 struct Args {
     // The number in the sequence to calculate
     #[arg(short, long, default_value_t = 0, allow_hyphen_values = true)]
-    number: i32
+    number: i32,
 }
 
 fn fib(nth: i32) -> anyhow::Result<BigInt> {
     match nth.cmp(&0) {
-        Ordering::Equal   => Ok(Zero::zero()),
-        Ordering::Less    => bail!("Number cannot be negative!"),
+        Ordering::Equal => Ok(Zero::zero()),
+        Ordering::Less => bail!("Number cannot be negative!"),
         Ordering::Greater => {
             let mut a: BigInt = One::one();
             let mut b: BigInt = One::one();
@@ -30,11 +30,24 @@ fn fib(nth: i32) -> anyhow::Result<BigInt> {
     }
 }
 
+// Format a BigInt value to a String with a thousands separator
+// Source: https://stackoverflow.com/a/67834588
+fn format_value(n: &BigInt) -> String {
+    n.to_string()
+        .as_bytes()
+        .rchunks(3)
+        .rev()
+        .map(std::str::from_utf8)
+        .collect::<Result<Vec<&str>, _>>()
+        .unwrap()
+        .join(",")
+}
+
 fn main() {
     let nth = Args::parse().number;
 
     match fib(nth) {
-        Ok(n)  => println!("{n}"),
-        Err(e) => eprintln!("ERROR: {e}")
+        Ok(n) => println!("{}", format_value(&n)),
+        Err(e) => eprintln!("ERROR: {e}"),
     }
 }
