c5aef88827433cc2029ecce53d8b5cc94fc786ff
Eric Griffith <griffith.eric@pm.me>
2025-08-16 10:58:19 -0500

    refactor: remove `format_value()` function, remove `#[arg()]` attribute from `Args::number`, change return type of `fib()` to `BigUint`

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,19 @@ use num_traits::{One, Zero};
 #[command(about)]
 struct Args {
     // The number in the sequence to calculate
-    #[arg(short, long, default_value_t = 0)]
     number: usize,
 }
 
-fn fib(nth: usize) -> String {
+fn fib(nth: usize) -> BigUint {
     let mut a: BigUint = Zero::zero();
     let mut b: BigUint = One::one();
 
     for _ in 0..nth {
-        let temp: BigUint = a + &b;
+        let temp = a + &b;
         a = b;
         b = temp;
     }
-    format_value(&a)
-}
-
-// Format a BigUint value to a String with a thousands separator
-// Source: https://stackoverflow.com/a/67834588
-fn format_value(n: &BigUint) -> String {
-    n.to_string()
-        .as_bytes()
-        .rchunks(3)
-        .rev()
-        .map(std::str::from_utf8)
-        .collect::<Result<Vec<&str>, _>>()
-        .unwrap()
-        .join(",")
+    a
 }
 
 fn main() {
